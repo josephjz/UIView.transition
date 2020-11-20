eb2589de9e7cf39516fd623efada66bd163f3a51
Jennifer Joseph <josephjz@bc.edu>
2020-11-20 12:34:19 -0500

    Completed App. Happy Halloween!

--- UIView.transition/ViewController.swift
+++ UIView.transition/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var delaySwitch: UISwitch!
+    @IBOutlet weak var ghostButton: UIButton!
+    @IBOutlet weak var pumpkinButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        emojiLabel.text = "" 
     }
-
-
+    
+    func displayEmoji(emoji: String) {
+        UIView.transition(with: emojiLabel, duration: 0.5, options: .transitionFlipFromBottom, animations: {
+            self.emojiLabel.text = "\(emoji)"
+        }, completion: nil)
+    }
+    
+    
+    @IBAction func ghostButtonPressed(_ sender: UIButton) {
+        if delaySwitch.isOn {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.displayEmoji(emoji: "👻")
+            }
+        } else {
+            displayEmoji(emoji: "👻")
+        }
+    }
+    
+    @IBAction func pumpkinButtonPressed(_ sender: UIButton) {
+        if delaySwitch.isOn {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                self.displayEmoji(emoji: "🎃")
+            }
+        } else {
+            displayEmoji(emoji: "🎃")
+        }
+    }
 }
 
